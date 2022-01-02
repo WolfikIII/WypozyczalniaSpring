3808b0e7f7ae88e39a0f9908385be87711a3fea9
Dawid Ząbek <zabek.dawid97@gmail.com>
2022-01-02 21:52:05 +0100

    wyciaganie hasla

--- src/main/java/com/example/logowaniespring/Start.java
+++ src/main/java/com/example/logowaniespring/Start.java
@@ -11,29 +11,38 @@ import java.util.Scanner;
 
 @Component
 public class Start {
-
+    Scanner scanner = new Scanner(System.in);
     private UserRepository userRepository;
     @Autowired
     public Start(UserRepository userRepository){
         this.userRepository = userRepository;
     }
+    public void changePass(){
+        System.out.println("Email: ");
+        String youremail = scanner.next();
+        System.out.println("Podaj nowe haslo: ");
+        String newpassword = scanner.next();
+        User takePass = userRepository.findByEmail(youremail);
+        takePass.setPassword(newpassword);
+        userRepository.save(takePass);
+        System.out.println("Twoje haslo zostalo zmienione!");
+    }
     @EventListener(ApplicationReadyEvent.class)
     public void run(){
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Login: ");
-        String log = scanner.next();
-        System.out.println("Password: ");
-        String pass = scanner.next();
-        System.out.println("Email: ");
-        String mail = scanner.next();
 
-       User user = new User(log, pass, mail);
-       userRepository.save(user);
-        System.out.println("dodano!");
-       userRepository.findByLogin(log);
-        System.out.println("znaleziono!");
+       // System.out.println("Login: ");
+        //String log = scanner.next();
+        //System.out.println("Password: ");
+        //String pass = scanner.next();
+
+
+       //User user = new User(log, pass, mail);
+       //userRepository.save(user);
+        //System.out.println("dodano!");
+       //userRepository.findByLogin(log);
+       // System.out.println("znaleziono!");
         //userRepository.userUpdate(user);
-        User takePass = userRepository.findByLogin(log);
-        System.out.println(takePass.getPassword());
+        changePass();
+
     }
 }
